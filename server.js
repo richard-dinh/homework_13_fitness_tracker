f54c7a23bddec4d111755df71ceb71c1cf4d78b8
richard-dinh <richarddinh@gmail.com>
2020-03-11 18:04:47 -0700

    wrote html routes

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(join(__dirname, 'public')))
 
+//html routes
+//homepage, rendering index.html
+app.get('/', (request, response) => {
+  response.sendFile(join(__dirname, 'public', 'index.html'))
+})
+
+//route for exercise.html
+app.get('/exercise', (request, response) => {
+  response.sendFile(join(__dirname, 'public', 'exercise.html'))
+})
+
+//route for stats.html
+app.get('/stats', (request, response) => {
+  response.sendFile(join(__dirname, 'public', 'stats.html'))
+})
 
 mongoose.connect('mongodb://localhost/workoutdb', {
   useNewUrlParser: true,
