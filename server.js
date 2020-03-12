5e5e29cff208e049feb8be99e04deebad3702e32
richard-dinh <richarddinh@gmail.com>
2020-03-11 17:44:25 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const {join} = require('path')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(join(__dirname, 'public')))
+
+
+mongoose.connect('mongodb://localhost/workoutdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then( () => app.listen(process.env.PORT || 3000))
+.catch( error => console.error(error))
